1bebce6e50a885d5db8a60e2a11969c38d530ab7
Huseyin Turkmenoglu <turkmenogluhuseyin@gmail.com>
2022-05-20 16:56:02 +0300

    Changed code format

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
     const cardValue = inputCard.current.value
       .replace(/\D/g, "")
       .match(/(\d{0,4})(\d{0,4})(\d{0,4})(\d{0,4})/);
+    
     inputCard.current.value = !cardValue[2]
       ? cardValue[1]
-      : `${cardValue[1]}-${cardValue[2]}${`${
-          cardValue[3] ? `-${cardValue[3]}` : ""
-        }`}${`${cardValue[4] ? `-${cardValue[4]}` : ""}`}`;
+      : `${cardValue[1]}-${cardValue[2]}${`${cardValue[3] ? `-${cardValue[3]}` : ""}`}${`${cardValue[4] ? `-${cardValue[4]}` : ""}`}`;
+    
     const numbers = inputCard.current.value.replace(/(\D)/g, "");
     setCard(numbers);
   };
